15ec39d4b713df4ce5fdc602034d6f1f1c27face
BonanzaInterective <devgitbonanza@gmail.com>
2023-10-03 19:03:59 +0530

    another commit

--- src/games/slotocrash/actions/custom_grid.ts
+++ src/games/slotocrash/actions/custom_grid.ts
@@ -1,5 +1,5 @@
 import BigNumber from "bignumber.js";
-import { IRandom, RandomObj } from "../../../libs/engine/generic/rng/Random";
+import { IRandom, RandomObj } from "../../../libs/engine/generic/rng/random";
 import { SlotSpinState } from "../../../libs/engine/slots/models/slot_state_model";
 import { RandomHelper } from "../../../libs/engine/slots/utils/random";
 import { SlotocrashMath } from "../models/slotocrash_math";
@@ -7,18 +7,18 @@ import { WeightedSymbols } from "../../../libs/engine/slots/models/slot_math_mod
 
 export class CustomGrid {
 
-    static AddBlastSymbol( rng :IRandom, state :SlotSpinState, math :SlotocrashMath ) {
-        const addBlast :any = RandomHelper.GetRandomFromList( rng, math.blastProb );
-        if ( addBlast.add ) {
-            const pos :RandomObj = rng.getRandom( new RandomObj(0, 3, -1) );
+    static AddBlastSymbol(rng: IRandom, state: SlotSpinState, math: SlotocrashMath) {
+        const addBlast: any = RandomHelper.GetRandomFromList(rng, math.blastProb);
+        if (addBlast.add) {
+            const pos: RandomObj = rng.getRandom(new RandomObj(0, 3, -1));
             state.initialGrid[pos.num][0] = math.blastSymbol;
         }
     }
 
-    static AddNewReel( rng :IRandom, state :SlotSpinState, math :SlotocrashMath, accumulated :number){
+    static AddNewReel(rng: IRandom, state: SlotSpinState, math: SlotocrashMath, accumulated: number) {
         const d = 10000;
-        let prob :number;
-        let symbols :WeightedSymbols[] = [];
+        let prob: number;
+        let symbols: WeightedSymbols[] = [];
         if (state.initialGrid.length < 10) {
             prob = state.multiplier * 0.2 / ((accumulated) + (state.multiplier * 0.2));
             symbols = math.symbols;
@@ -30,12 +30,12 @@ export class CustomGrid {
             throw new Error("prob " + prob);
         }
         prob *= d;
-        const value :RandomObj = rng.getRandom( new RandomObj(0, d, -1) )
-        if ( value.num <= prob ) {
-            state.initialGrid.push( [math.blastSymbol] );
+        const value: RandomObj = rng.getRandom(new RandomObj(0, d, -1))
+        if (value.num <= prob) {
+            state.initialGrid.push([math.blastSymbol]);
         } else {
-            const symbol :WeightedSymbols = RandomHelper.GetRandomFromList(rng, symbols) as WeightedSymbols;
-            state.initialGrid.push( [ symbol.symbol] );
+            const symbol: WeightedSymbols = RandomHelper.GetRandomFromList(rng, symbols) as WeightedSymbols;
+            state.initialGrid.push([symbol.symbol]);
         }
     }
 
