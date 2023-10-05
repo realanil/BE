e1c18a1cb719f62320ab66fd62536530b21883ac
BonanzaInterective <devgitbonanza@gmail.com>
2023-10-05 17:21:41 +0530

    revert to old code

--- src/libs/engine/slots/models/slot_math_model.ts
+++ src/libs/engine/slots/models/slot_math_model.ts
@@ -3,42 +3,31 @@ import BigNumber from "bignumber.js";
 export class SlotMath {
     public info : SlotInfoMath = new SlotInfoMath();
     public paidReels : SlotReelsMath[] = [];
-    public freeReels : SlotReelsMath[] = [];
-    public paidFeatureReels : SlotReelsMath[] = [];
     public conditions :SlotConditionMath[] = [];
-    public collection :SlotCollectionMath[][] = []; 
     public actions :SlotActionMath[] = [];
     public defaultgrid :number[][] = [];
 
     protected bd( v : number) : BigNumber { return new BigNumber( v); } 
 }
 
-class SlotCollectionMath {
-    public level :string;
-    public from? :number;
-    public to? :number;
-}
-
 export class SlotInfoMath {
-    public betMultiplier :BigNumber = new BigNumber(0);
-    public gridLayout :number[] = [];
-    public wildSymbols :number[] = [];
-    public payLines :number[][] = [];
-    public symbols :SlotSymbolsMath[] = [];
+    public betMultiplier : BigNumber = new BigNumber(0);
+    public gridLayout : number[] = [];
+    public wildSymbols : number[] = [];
+    public payLines : number[][] = [];
+    public symbols : SlotSymbolsMath[] = [];
 }
 
 class SlotSymbolsMath {
     public payout : BigNumber[] = [];
     public name : string = "";
     public id : number = -1;
-    public key ?:string;
 }
 
 class SlotReelsMath {
     public id : string = "";
-    public reels? : number[][] = [];
-    public symbols? :WeightedSymbols[] = [];
-    public weight? :number = -1;
+    public reels : number[][] = [];
+    public symbols :WeightedSymbols[] = [];
 }
 
 export class WeightedSymbols{
@@ -46,32 +35,17 @@ export class WeightedSymbols{
     public weight :number = -1;
 }
 
-export class IsAvailable {
-    public available :boolean = false;
-    public weight :number = -1;
-}
-
 export class SlotConditionMath {
-    public symbol? :number = -1;
+    public symbol :number = -1;
     public id :string = null;
-    public oak? :number[] = null;
-    public minCount? :number = -1;
-    public maxCount? :number = -1;
-    public isAvailable? :IsAvailable[];
+    public oak :number[] = null;
+    public minCount :number = -1;
+    public maxCount :number = -1;
 }
 
 export class SlotActionMath {
-    public payout? :BigNumber = new BigNumber(0);
-    public triggers? :string[] = [];
-    public spins? :number = -1;
-    public features? :SlotFeatureMath[][] = null;
-    public feature? :SlotFeatureMath[] = null;
-    public id? :string = null;
-}
-
-class SlotFeatureMath { 
-    public weight :number = 0;
-    public id :string ;
-    public symbols? :number[];
+    public payout :BigNumber = new BigNumber(0);
+    public triggers :string[] = [];
+    public spins :number = -1;
 }
 
--- src/libs/server/rgs.ts
+++ src/libs/server/rgs.ts
@@ -7,58 +7,58 @@ import cors from 'cors';
 
 export class RGS {
 
-    states :Map<string, any> = new Map();
-    
-    app: express.Application ;
-    server : BaseSlotGame
+    states: Map<string, any> = new Map();
 
-    constructor( gameServer : BaseSlotGame){
+    app: express.Application;
+    server: BaseSlotGame
+
+    constructor(gameServer: BaseSlotGame) {
         this.app = express();
-        this.app.use( express.json() );
+        this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }))
-        this.app.use( cors({origin:'*'}))
+        this.app.use(cors({ origin: '*' }))
 
         this.server = gameServer;
 
-        this.app.post('/:gameCode/config', (req :Request, res :Response)=> {
+        this.app.post('/:gameCode/config', (req: Request, res: Response) => {
             const gameCode = req.params.gameCode;
-            const config :any = this.server.config( null);
+            const config: any = this.server.config(null);
             config.sessionid = uuidv4();
 
-            if (this.states.keys.length > 300){
+            if (this.states.keys.length > 300) {
                 Array.from(this.states.keys())
-                .slice(0, 300 - this.states.keys.length )
-                .forEach(key => this.states.delete(key))
+                    .slice(0, 300 - this.states.keys.length)
+                    .forEach(key => this.states.delete(key))
             }
 
-            this.states.set( config.sessionid, null);
-            res.send( config ).status(200);
+            this.states.set(config.sessionid, null);
+            res.send(config).status(200);
         });
-        this.app.post('/:gameCode/play', (req :Request, res :Response) => {
+        this.app.post('/:gameCode/play', (req: Request, res: Response) => {
             const gameCode = req.params.gameCode;
             const sessionid = req.body.sessionid;
 
-            if (!this.states.has(sessionid) ){
-                res.send( `invalid session id ${sessionid}`).status( 200);
+            if (!this.states.has(sessionid)) {
+                res.send(`invalid session id ${sessionid}`).status(200);
                 return;
             }
             const state = this.states.get(sessionid);
 
-            const stake :BigNumber = req.body.stake ? new BigNumber( req.body.stake) : new BigNumber(0);
-            
-            const request = { action:req.body.action , stake:stake, cheat:req.body.cheat, state };
-            const response :ServerResponseModel = this.server.play( request );
+            const stake: BigNumber = req.body.stake ? new BigNumber(req.body.stake) : new BigNumber(0);
+
+            const request = { action: req.body.action, stake: stake, cheat: req.body.cheat, state };
+            const response: ServerResponseModel = this.server.play(request);
             if (response.state) {
-                this.states.set( sessionid, JSON.parse( JSON.stringify( response.state ) ));
+                this.states.set(sessionid, JSON.parse(JSON.stringify(response.state)));
             }
-            res.send( response.response).status( 200);
+            res.send(response.response).status(200);
         });
     }
 
-    start(port : string){
+    start(port: string) {
         this.app.listen(port, () => {
             console.log(`listening at port ${port} `);
         });
     }
-    
+
 }
