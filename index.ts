2d773fd02cd81c00bd1196b1e9a93e010841c0cc
BonanzaInterective <devgitbonanza@gmail.com>
2023-10-05 12:55:22 +0530

    port number changed

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { SlotocrashServer } from "./src/games/slotocrash/slotocrash_server";
 import { RGS } from "./src/libs/server/rgs";
 
-const port: string = "8080";
+const port: string = "9090";
 
 const server = new RGS(new SlotocrashServer());
 server.start(port);
