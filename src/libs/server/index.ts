e98cb76fc1b175700d24183ac8abc90106bdaf20
realanil <46739690+realanil@users.noreply.github.com>
2024-01-16 18:25:55 +0530

    Update index.ts

--- src/libs/server/index.ts
+++ src/libs/server/index.ts
@@ -10,10 +10,10 @@ const port: string = process.env.PORT ? process.env.PORT : "8080";
 glob(`./src/games/*/*.ts`).then(async servFiles => {
 
     const engines: Map<string, BaseSlotGame> = new Map();
-    console.log("Hello");
+    console.log("servFiles", servFiles);
 
     for (let i = 0; i < servFiles.length; i++) {
-        console.log(servFiles, "servFiles");
+        console.log(servFiles[i], i, "servFiles");
         const tsfile = servFiles[i].split('src\\')[1];
         console.log(tsfile, "tsfile");
         const servClass = tsfile.split(".")[0]
