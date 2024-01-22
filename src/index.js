718cf3b06b52a2483f9290876bd64be9c87e88af
BonanzaInterective <dev1@bonanzainteractive.com>
2024-01-22 16:37:02 +0530

    SC Commit

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var slotocrash_server_1 = require("./games/slotocrash/slotocrash_server");
-var rgs_1 = require("./libs/server/rgs");
-var port = "8080";
-var server = new rgs_1.RGS(new slotocrash_server_1.SlotocrashServer());
-server.start(port);
