6d417f850e29cc2e23bf9a24a0d9644d9264388f
BonanzaInterective <dev1@bonanzainteractive.com>
2024-02-07 14:45:08 +0530

    commit 1

--- src/games/viking/models/viking_state.ts
+++
@@ -1,6 +0,0 @@
-import { SlotState } from "../../../libs/engine/slots/models/slot_state_model";
-
-export class VikingState extends SlotState {
-    public scatterCount:number;
-    public activeReels: boolean[];
-}
