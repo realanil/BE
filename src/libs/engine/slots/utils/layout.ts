76d6626dc0e431c405358013a6688087bdf7b45b
BonanzaInterective <dev1@bonanzainteractive.com>
2024-02-29 16:27:37 +0530

    another commit

--- src/libs/engine/slots/utils/layout.ts
+++ src/libs/engine/slots/utils/layout.ts
@@ -2,16 +2,16 @@ import { IRandom } from "../../generic/rng/random";
 import { RandomHelper } from "./random";
 
 interface WeightedLayout {
-    collections: { count:number, weight:number}[] 
+    collections: { count: number, weight: number }[]
 }
 
 export class Layout {
 
-    static weightedLayout( rng:IRandom, details: WeightedLayout[] ) :number[] {
-        const layout:number[] = [];
+    static weightedLayout(rng: IRandom, details: WeightedLayout[]): number[] {
+        const layout: number[] = [];
 
-        details.forEach( (detail, index) => { 
-            const selected:any = RandomHelper.GetRandomFromList( rng, detail.collections );
+        details.forEach((detail, index) => {
+            const selected: any = RandomHelper.GetRandomFromList(rng, detail.collections);
             layout[index] = selected.count;
         });
 
