ad7e0a91050fb52c4c2f3779bb454435f7f20240
BonanzaInterective <dev1@bonanzainteractive.com>
2024-02-29 15:49:54 +0530

    reset commit

--- src/games/fairyleaf/models/shinningcrown_state.ts
+++
@@ -1,9 +0,0 @@
-import { SlotState } from "../../../libs/engine/slots/models/slot_state_model";
-
-export class ShinningCrownState extends SlotState {
-
-    public cashPrizes: { offset: number, id: string, multiplier: number }[] = [];
-    public cashPrizesMath = [];
-    public awardedJackpots = [];
-
-}
